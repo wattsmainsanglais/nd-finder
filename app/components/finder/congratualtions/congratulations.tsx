920b8f7a7238d20de6ec6872f3a04dfb3552b3ac
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-02-09 10:33:53 +0100

    disable eslint rules for build

--- app/components/finder/congratualtions/congratulations.tsx
+++ app/components/finder/congratualtions/congratulations.tsx
@@ -1,3 +1,5 @@
+ /* eslint react/no-unescaped-entities */
+
 import React from 'react'
 import style from './congratulations.module.css'
 import { indie } from '../layout'
@@ -9,7 +11,7 @@ export interface PointsProps {
 export default function Congratulations({points}: PointsProps ){
 
     return(
-     <section className={`${style.congratulationsWrapper} ${indie.className} ` }>
+     <section className={`${style.congratulationsWrapper} ${indie.className}` }>
 
         <h1>Congratualtions!</h1>
         <h2>You've reached the end of the game</h2>
