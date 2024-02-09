8611bf4a4967fb99f493846955d7a4aca2432bd4
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-02-09 14:41:47 +0100

    added bundle analyzer, app is suprisingly streamline :)

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
+
+
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
-
-module.exports = {
-    eslint: {
+module.exports = withBundleAnalyzer( nextConfig, {
+  
+   eslint: {
       // Warning: This allows production builds to successfully complete even if
       // your project has ESLint errors.
       ignoreDuringBuilds: true,
     },
-  }
+})
+
