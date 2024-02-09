fc2a48c3401de3774b67c63a29f6ad284f7c7935
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-02-09 09:23:24 +0100

    Add loading.tsx component for finders directory

---
+++ app/components/finder/loading.tsx
@@ -0,0 +1,17 @@
+
+import LoadingIcon from './loadingIcon'
+
+import styles from '../../layout.module.css'
+
+export default function Loading(){
+      return( 
+        <section className={styles.loadingWrapper}>
+          
+          <LoadingIcon/>
+
+         
+          
+          
+        </section>
+      )
+}
